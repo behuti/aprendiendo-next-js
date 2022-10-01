a92b40c1ba4a0cd3301dbab1d5201c58f30ea32f
Eduardo Henao <echenao@gmail.com>
2022-10-01 15:46:13 -0500

    Add pages

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>Esta es la página de about</div>
+  )
+}
+
+export default about
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hola Platzi! asjhflk</h1>
+    </div>
+  );
+};
+
+export default Home;
